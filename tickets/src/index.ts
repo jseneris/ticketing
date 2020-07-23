20f19d5dbccb51033ff859d4f876127cef5bbc3b
John Seneris <john@covetlyapp.com>
2020-07-23 09:39:03 -0400

    adding startup messages to services

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -5,7 +5,7 @@ import { OrderCreatedListener } from './events/listeners/order-created-listener'
 import { OrderCancelledListener } from './events/listeners/order-cancelled-listener';
 
 const start = async () => {
-  console.log('starting...');
+  console.log('Starting Tickets service...');
 
   if (!process.env.JWT_KEY) {
     throw new Error('JWT_Key must be defined');
